6394e35f7829b4baff68ee18b96dcf26f2074403
amedama41 <kamo.tanabota@gmail.com>
2014-09-01 22:08:03 +0900

    Add header include dependency to unit test Makefile

--- test/oxm_match_field_test.cpp
+++
@@ -1,6 +0,0 @@
-#define BOOST_TEST_DYN_LINK
-#ifndef BOOST_TEST_MODULE
-#define BOOST_TEST_MODULE oxm_match_field_test
-#endif
-#include <boost/test/unit_test.hpp>
-
--- test/utility_test.cpp
+++
@@ -1,6 +0,0 @@
-#define BOOST_TEST_DYN_LINK
-#ifndef BOOST_TEST_MODULE
-#define BOOST_TEST_MODULE utility_test
-#endif
-#include <boost/test/unit_test.hpp>
-
