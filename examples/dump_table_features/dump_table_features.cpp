55e6b8ee28a3a1850644ba296315bad545b9bcb2
amedama41 <kamo.tanabota@gmail.com>
2014-09-03 21:45:26 +0900

    Add a table features example

---
+++ examples/dump_table_features/dump_table_features.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <canard/network/protocol/openflow/v13.hpp>
+
+namespace of = canard::network::openflow::v13;
+
+struct dump_table_features;
+using controller = of::controller<dump_table_features>;
+
+struct dump_table_features
+{
+    void handle(controller::channel_ptr channel)
+    {
+        channel->send(of::table_features_request{});
+    }
+
+    void handle(controller::channel_ptr channel, of::table_features_reply reply)
+    {
+        std::cout << reply << std::endl;
+    }
+
+    template <class... Args>
+    void handle(Args const&...) {}
+};
+
+int main()
+{
+    auto handler = dump_table_features{};
+    controller cont{controller::options{handler}.address("0.0.0.0")};
+    try {
+        cont.run();
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
