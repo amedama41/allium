ebbbf33fd3f3e5fc926db5ab4a4606a0b6a881b8
amedama41 <kamo.tanabota@gmail.com>
2016-01-23 17:07:54 +0900

    rewrite v13 repeater_hub example

--- examples/repeater_hub/repeater_hub.cpp
+++ examples/repeater_hub/repeater_hub.cpp
@@ -2,41 +2,43 @@
 #include <canard/network/protocol/openflow/v13.hpp>
 #include "../oxm_match_creator.hpp"
 
-namespace of = canard::network::openflow::v13;
-
-struct repeater_hub;
-using controller = of::controller<repeater_hub>;
+namespace of = canard::network::openflow;
+namespace v13 = of::v13;
 
 struct repeater_hub
 {
-
-    void handle(controller::channel_ptr channel)
+    template <class Channel>
+    void handle(Channel const& channel, of::hello const&)
     {
-        channel->send(of::flow_mod_add{{
-                  of::flow_entry_id::table_miss()
-                , of::instructions::write_actions{of::actions::output::to_controller()}
-        }, 0, of::protocol::OFPFF_SEND_FLOW_REM});
+        channel->async_send(v13::flow_mod_add{{
+                  v13::flow_entry_id::table_miss()
+                , v13::instructions::write_actions{v13::actions::output::to_controller()}
+        }, 0, v13::protocol::OFPFF_SEND_FLOW_REM});
     }
 
-    void handle(controller::channel_ptr channel, of::packet_in pkt_in)
+    template <class Channel>
+    void handle(Channel const& channel, v13::packet_in pkt_in)
     {
-        channel->send(of::flow_mod_add{{
+        channel->async_send(v13::flow_mod_add{{
                   {oxm_match_from_packet(pkt_in.frame()), 65535}
-                , of::instructions::write_actions{of::actions::output{of::protocol::OFPP_ALL}}
-        }, 0, of::protocol::OFPFF_SEND_FLOW_REM});
-        channel->send(of::packet_out{pkt_in.frame()
-                , of::protocol::OFPP_CONTROLLER, of::actions::output{of::protocol::OFPP_ALL}});
+                , v13::instructions::write_actions{v13::actions::output{v13::protocol::OFPP_ALL}}
+        }, 0, v13::protocol::OFPFF_SEND_FLOW_REM});
+        channel->async_send(v13::packet_out{pkt_in.frame()
+                , v13::protocol::OFPP_CONTROLLER, v13::actions::output{v13::protocol::OFPP_ALL}});
     }
 
-    template <class... Args>
-    void handle(controller::channel_ptr, Args const&...) {}
+    template <class Channel, class... Args>
+    void handle(Channel const&, Args const&...) {}
 };
 
 int main()
 {
     auto handler = repeater_hub{};
-    controller cont{controller::options{handler}.address("0.0.0.0")};
+    using controller = v13::controller<repeater_hub>;
     try {
+        controller cont{
+            controller::options{handler}.address("0.0.0.0")
+        };
         cont.run();
     }
     catch (std::exception& e) {
