f7dc4097682c3d824f1de7c10696dba6c596f12c
amedama41 <kamo.tanabota@gmail.com>
2016-01-30 16:50:27 +0900

    rewrite barrier messages

--- include/canard/network/protocol/openflow/v10/message/barrier.hpp
+++ include/canard/network/protocol/openflow/v10/message/barrier.hpp
@@ -3,10 +3,11 @@
 
 #include <cstdint>
 #include <stdexcept>
-#include <canard/network/protocol/openflow/v10/detail/basic_openflow_message.hpp>
 #include <canard/network/protocol/openflow/detail/decode.hpp>
 #include <canard/network/protocol/openflow/detail/encode.hpp>
 #include <canard/network/protocol/openflow/get_xid.hpp>
+#include <canard/network/protocol/openflow/v10/detail/basic_openflow_message.hpp>
+#include <canard/network/protocol/openflow/v10/detail/byteorder.hpp>
 #include <canard/network/protocol/openflow/v10/openflow.hpp>
 
 namespace canard {
@@ -15,103 +16,115 @@ namespace openflow {
 namespace v10 {
 namespace messages {
 
+    namespace barrier_detail {
+
+        template <class T>
+        class barrier_base
+            : public v10_detail::basic_openflow_message<T>
+        {
+        protected:
+            explicit barrier_base(std::uint32_t const xid) noexcept
+                : header_{
+                      protocol::OFP_VERSION
+                    , T::message_type
+                    , sizeof(v10_detail::ofp_header)
+                    , xid
+                  }
+            {
+            }
+
+        public:
+            auto header() const noexcept
+                -> v10_detail::ofp_header const&
+            {
+                return header_;
+            }
+
+            template <class Container>
+            auto encode(Container& container) const
+                -> Container&
+            {
+                return detail::encode(container, header_);
+            }
+
+            template <class Iterator>
+            static auto decode(Iterator& first, Iterator last)
+                -> T
+            {
+                return T{detail::decode<v10_detail::ofp_header>(first, last)};
+            }
+
+            static void validate(v10_detail::ofp_header const& header)
+            {
+                if (header.version != protocol::OFP_VERSION) {
+                    throw std::runtime_error{"invalid version"};
+                }
+                if (header.type != T::message_type) {
+                    throw std::runtime_error{"invalid message type"};
+                }
+                if (header.length != sizeof(v10_detail::ofp_header)) {
+                    throw std::runtime_error{"invalid length"};
+                }
+            }
+
+        protected:
+            explicit barrier_base(v10_detail::ofp_header const& header) noexcept
+                : header_(header)
+            {
+            }
+
+        private:
+            v10_detail::ofp_header header_;
+        };
+
+    } // namespace barrier_detail
+
     class barrier_request
-        : public v10_detail::basic_openflow_message<barrier_request>
+        : public barrier_detail::barrier_base<barrier_request>
     {
     public:
-        static protocol::ofp_type const message_type
+        static constexpr protocol::ofp_type message_type
             = protocol::OFPT_BARRIER_REQUEST;
 
-        explicit barrier_request(std::uint32_t const xid = get_xid())
-            : header_{protocol::OFP_VERSION, message_type, sizeof(header_), xid}
+        explicit barrier_request(std::uint32_t const xid = get_xid()) noexcept
+            : barrier_base{xid}
         {
-        }
-
-        auto header() const
-            -> v10_detail::ofp_header
-        {
-            return header_;
-        }
-
-        template <class Container>
-        auto encode(Container& container) const
-            -> Container&
-        {
-            return detail::encode(container, header_);
-        }
-
-        template <class Iterator>
-        static auto decode(Iterator& first, Iterator last)
-            -> barrier_request
-        {
-            auto const header = detail::decode<v10_detail::ofp_header>(first, last);
-            return barrier_request{header};
         }
 
     private:
-        explicit barrier_request(v10_detail::ofp_header const header)
-            : header_(header)
-        {
-            if (version() != protocol::OFP_VERSION) {
-                throw std::runtime_error{"invalid version"};
-            }
-            if (type() != message_type) {
-                throw std::runtime_error{"invalid message type"};
-            }
-        }
+        friend barrier_base;
 
-    private:
-        v10_detail::ofp_header header_;
+        explicit barrier_request(v10_detail::ofp_header const& header) noexcept
+            : barrier_base{header}
+        {
+        }
     };
 
 
     class barrier_reply
-        : public v10_detail::basic_openflow_message<barrier_reply>
+        : public barrier_detail::barrier_base<barrier_reply>
     {
     public:
-        static protocol::ofp_type const message_type
+        static constexpr protocol::ofp_type message_type
             = protocol::OFPT_BARRIER_REPLY;
 
+        explicit barrier_reply(std::uint32_t const xid = get_xid()) noexcept
+            : barrier_base{xid}
+        {
+        }
+
         explicit barrier_reply(barrier_request const& request)
-            : header_{protocol::OFP_VERSION, message_type, sizeof(header_), request.xid()}
+            : barrier_reply{request.xid()}
         {
-        }
-
-        auto header() const
-            -> v10_detail::ofp_header
-        {
-            return header_;
-        }
-
-        template <class Container>
-        auto encode(Container& container) const
-            -> Container&
-        {
-            return detail::encode(container, header_);
-        }
-
-        template <class Iterator>
-        static auto decode(Iterator& first, Iterator last)
-            -> barrier_reply
-        {
-            auto const header = detail::decode<v10_detail::ofp_header>(first, last);
-            return barrier_reply{header};
         }
 
     private:
-        explicit barrier_reply(v10_detail::ofp_header const header)
-            : header_(header)
-        {
-            if (version() != protocol::OFP_VERSION) {
-                throw std::runtime_error{"invalid version"};
-            }
-            if (type() != message_type) {
-                throw std::runtime_error{"invalid message type"};
-            }
-        }
+        friend barrier_base;
 
-    private:
-        v10_detail::ofp_header header_;
+        explicit barrier_reply(v10_detail::ofp_header const& header) noexcept
+            : barrier_base{header}
+        {
+        }
     };
 
 } // namespace messages
--- include/canard/network/protocol/openflow/v13/message/barrier.hpp
+++ include/canard/network/protocol/openflow/v13/message/barrier.hpp
@@ -1,8 +1,11 @@
-#ifndef CANARD_NETWORK_OPENFLOW_V13_BARRIER_HPP
-#define CANARD_NETWORK_OPENFLOW_V13_BARRIER_HPP
+#ifndef CANARD_NETWORK_OPENFLOW_V13_MESSAGES_BARRIER_HPP
+#define CANARD_NETWORK_OPENFLOW_V13_MESSAGES_BARRIER_HPP
 
+#include <cstdint>
+#include <stdexcept>
 #include <canard/network/protocol/openflow/detail/decode.hpp>
 #include <canard/network/protocol/openflow/detail/encode.hpp>
+#include <canard/network/protocol/openflow/get_xid.hpp>
 #include <canard/network/protocol/openflow/v13/detail/basic_openflow_message.hpp>
 #include <canard/network/protocol/openflow/v13/detail/byteorder.hpp>
 #include <canard/network/protocol/openflow/v13/openflow.hpp>
@@ -13,117 +16,121 @@ namespace openflow {
 namespace v13 {
 namespace messages {
 
+    namespace barrier_detail {
+
+        template <class T>
+        class barrier_base
+            : public v13_detail::basic_openflow_message<T>
+        {
+        protected:
+            explicit barrier_base(std::uint32_t const xid) noexcept
+                : header_{
+                      protocol::OFP_VERSION
+                    , T::message_type
+                    , sizeof(v13_detail::ofp_header)
+                    , xid
+                  }
+            {
+            }
+
+        public:
+            auto header() const noexcept
+                -> v13_detail::ofp_header const&
+            {
+                return header_;
+            }
+
+            template <class Container>
+            auto encode(Container& container) const
+                -> Container&
+            {
+                return detail::encode(container, header_);
+            }
+
+            template <class Iterator>
+            static auto decode(Iterator& first, Iterator last)
+                -> T
+            {
+                return T{detail::decode<v13_detail::ofp_header>(first, last)};
+            }
+
+            static void validate(v13_detail::ofp_header const& header)
+            {
+                if (header.version != protocol::OFP_VERSION) {
+                    throw std::runtime_error{"invalid version"};
+                }
+                if (header.type != T::message_type) {
+                    throw std::runtime_error{"invalid message type"};
+                }
+                if (header.length != sizeof(v13_detail::ofp_header)) {
+                    throw std::runtime_error{"invalid length"};
+                }
+            }
+
+        protected:
+            explicit barrier_base(v13_detail::ofp_header const& header) noexcept
+                : header_(header)
+            {
+            }
+
+        private:
+            v13_detail::ofp_header header_;
+        };
+
+    } // namespace barrier_detail
+
     class barrier_request
-        : public v13_detail::basic_openflow_message<barrier_request>
+        : public barrier_detail::barrier_base<barrier_request>
     {
     public:
-        static protocol::ofp_type const message_type
+        static constexpr protocol::ofp_type message_type
             = protocol::OFPT_BARRIER_REQUEST;
 
-        barrier_request()
-            : header_{
-                  protocol::OFP_VERSION, message_type
-                , sizeof(v13_detail::ofp_header), get_xid()
-              }
-        {
-        }
-
-        auto header() const
-            -> v13_detail::ofp_header const&
-        {
-            return header_;
-        }
-
-        using basic_openflow_message::encode;
-
-        template <class Container>
-        auto encode(Container& container) const
-            -> Container&
-        {
-            return detail::encode(container, header_);
-        }
-
-        template <class Iterator>
-        static auto decode(Iterator& first, Iterator last)
-            -> barrier_request
-        {
-            auto header = detail::decode<v13_detail::ofp_header>(first, last);
-            if (header.length != sizeof(v13_detail::ofp_header) || std::distance(first, last) != 0) {
-                throw 2;
-            }
-            return barrier_request{header};
-        }
-
-    private:
-        barrier_request(v13_detail::ofp_header const& header)
-            : header_(header)
+        explicit barrier_request(std::uint32_t const xid = get_xid()) noexcept
+            : barrier_base{xid}
         {
         }
 
     private:
-        v13_detail::ofp_header header_;
+        friend barrier_base;
+
+        explicit barrier_request(v13_detail::ofp_header const& header) noexcept
+            : barrier_base{header}
+        {
+        }
     };
 
+
     class barrier_reply
-        : public v13_detail::basic_openflow_message<barrier_reply>
+        : public barrier_detail::barrier_base<barrier_reply>
     {
     public:
-        static protocol::ofp_type const message_type
+        static constexpr protocol::ofp_type message_type
             = protocol::OFPT_BARRIER_REPLY;
 
-        barrier_reply(barrier_request const& request)
-            : header_{
-                  protocol::OFP_VERSION, message_type
-                , sizeof(v13_detail::ofp_header), request.xid()
-              }
+        explicit barrier_reply(std::uint32_t const xid = get_xid()) noexcept
+            : barrier_base{xid}
         {
         }
 
-        auto header() const
-            -> v13_detail::ofp_header const&
-        {
-            return header_;
-        }
-
-        using basic_openflow_message::encode;
-
-        template <class Container>
-        auto encode(Container& container) const
-            -> Container&
-        {
-            return detail::encode(container, header_);
-        }
-
-    public:
-        template <class Iterator>
-        static auto decode(Iterator& first, Iterator last)
-            -> barrier_reply
-        {
-            auto const header = detail::decode<v13_detail::ofp_header>(first, last);
-            if (header.length != sizeof(v13_detail::ofp_header) && std::distance(first, last) != 0) {
-                throw 2;
-            }
-            return barrier_reply{header};
-        }
-
-    private:
-        explicit barrier_reply(v13_detail::ofp_header const& header)
-            : header_(header)
+        explicit barrier_reply(barrier_request const& request)
+            : barrier_reply{request.xid()}
         {
         }
 
     private:
-        v13_detail::ofp_header header_;
+        friend barrier_base;
+
+        explicit barrier_reply(v13_detail::ofp_header const& header) noexcept
+            : barrier_base{header}
+        {
+        }
     };
 
 } // namespace messages
-
-using messages::barrier_request;
-using messages::barrier_reply;
-
 } // namespace v13
 } // namespace openflow
 } // namespace network
 } // namespace canard
 
-#endif // CANARD_NETWORK_OPENFLOW_V13_BARRIER_HPP
+#endif // CANARD_NETWORK_OPENFLOW_V13_MESSAGES_BARRIER_HPP
