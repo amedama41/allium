22930bcac2cc27fbea163bb0b0ad30df231d39f3
amedama41 <kamo.tanabota@gmail.com>
2016-02-12 22:21:53 +0900

    rewrite flow_removed messages

--- include/canard/network/protocol/openflow/v10/message/flow_removed.hpp
+++ include/canard/network/protocol/openflow/v10/message/flow_removed.hpp
@@ -2,12 +2,14 @@
 #define CANARD_NETWORK_OPENFLOW_V10_MESSAGES_FLOW_REMOVED_HPP
 
 #include <cstdint>
-#include <chrono>
 #include <stdexcept>
 #include <canard/network/protocol/openflow/detail/decode.hpp>
 #include <canard/network/protocol/openflow/detail/encode.hpp>
+#include <canard/network/protocol/openflow/get_xid.hpp>
 #include <canard/network/protocol/openflow/v10/detail/basic_openflow_message.hpp>
+#include <canard/network/protocol/openflow/v10/detail/byteorder.hpp>
 #include <canard/network/protocol/openflow/v10/detail/flow_entry_adaptor.hpp>
+#include <canard/network/protocol/openflow/v10/flow_entry.hpp>
 #include <canard/network/protocol/openflow/v10/match_set.hpp>
 #include <canard/network/protocol/openflow/v10/openflow.hpp>
 
@@ -19,58 +21,73 @@ namespace messages {
 
     class flow_removed
         : public v10_detail::basic_openflow_message<flow_removed>
-        , public v10_detail::flow_entry_adaptor<flow_removed, v10_detail::ofp_flow_removed>
+        , public v10_detail::flow_entry_adaptor<
+                flow_removed, v10_detail::ofp_flow_removed
+          >
     {
     public:
-        static protocol::ofp_type const message_type
+        static constexpr protocol::ofp_type message_type
             = protocol::OFPT_FLOW_REMOVED;
 
-        auto header() const
-            -> v10_detail::ofp_header
+        flow_removed(
+                  match_set const& match
+                , std::uint16_t const priority
+                , std::uint64_t const cookie
+                , protocol::ofp_flow_removed_reason const reason
+                , v10::elapsed_time const elapsed_time
+                , v10::timeouts const& timeouts
+                , v10::counters const& counters
+                , std::uint32_t const xid = get_xid())
+            : flow_removed_{
+                  v10_detail::ofp_header{
+                      protocol::OFP_VERSION
+                    , message_type
+                    , sizeof(v10_detail::ofp_flow_removed)
+                    , xid
+                  }
+                , match.ofp_match()
+                , cookie
+                , priority
+                , std::uint8_t(reason)
+                , { 0 }
+                , elapsed_time.duration_sec()
+                , elapsed_time.duration_nsec()
+                , timeouts.idle_timeout()
+                , { 0, 0 }
+                , counters.packet_count()
+                , counters.byte_count()
+              }
+        {
+        }
+
+        flow_removed(
+                  flow_entry const& entry
+                , protocol::ofp_flow_removed_reason const reason
+                , v10::elapsed_time const elapsed_time
+                , v10::timeouts const& timeouts
+                , v10::counters const& counters
+                , std::uint32_t const xid = get_xid())
+            : flow_removed{
+                  entry.match(), entry.priority(), entry.cookie()
+                , reason
+                , elapsed_time
+                , timeouts
+                , counters
+                , xid
+              }
+        {
+        }
+
+        auto header() const noexcept
+            -> v10_detail::ofp_header const&
         {
             return flow_removed_.header;
         }
 
-        auto reason() const
+        auto reason() const noexcept
             -> protocol::ofp_flow_removed_reason
         {
             return protocol::ofp_flow_removed_reason(flow_removed_.reason);
-        }
-
-        auto duration_sec() const
-            -> std::chrono::seconds
-        {
-            return std::chrono::seconds{flow_removed_.duration_sec};
-        }
-
-        auto duration_nsec() const
-            -> std::chrono::nanoseconds
-        {
-            return std::chrono::nanoseconds{flow_removed_.duration_nsec};
-        }
-
-        auto ofp_flow_entry() const
-            -> v10_detail::ofp_flow_removed const&
-        {
-            return flow_removed_;
-        }
-
-        auto is_idle_timeout() const
-            -> bool
-        {
-            return reason() == protocol::OFPRR_IDLE_TIMEOUT;
-        }
-
-        auto is_hard_timeout() const
-            -> bool
-        {
-            return reason() == protocol::OFPRR_HARD_TIMEOUT;
-        }
-
-        auto is_deleted() const
-            -> bool
-        {
-            return reason() == protocol::OFPRR_DELETE;
         }
 
         template <class Container>
@@ -84,21 +101,37 @@ namespace messages {
         static auto decode(Iterator& first, Iterator last)
             -> flow_removed
         {
-            auto const removed
-                = detail::decode<v10_detail::ofp_flow_removed>(first, last);
-            return flow_removed{removed};
+            return flow_removed{
+                detail::decode<v10_detail::ofp_flow_removed>(first, last)
+            };
+        }
+
+        static void validate(v10_detail::ofp_header const& header)
+        {
+            if (header.version != protocol::OFP_VERSION) {
+                throw std::runtime_error{"invalid version"};
+            }
+            if (header.type != message_type) {
+                throw std::runtime_error{"invalid message type"};
+            }
+            if (header.length != sizeof(v10_detail::ofp_flow_removed)) {
+                throw std::runtime_error{"invalid length"};
+            }
         }
 
     private:
-        explicit flow_removed(v10_detail::ofp_flow_removed const& removed)
+        explicit flow_removed(
+                v10_detail::ofp_flow_removed const& removed) noexcept
             : flow_removed_(removed)
         {
-            if (version() != protocol::OFP_VERSION) {
-                throw std::runtime_error{"invalid version"};
-            }
-            if (type() != message_type) {
-                throw std::runtime_error{"invalid message type"};
-            }
+        }
+
+        friend flow_entry_adaptor;
+
+        auto ofp_flow_entry() const noexcept
+            -> v10_detail::ofp_flow_removed const&
+        {
+            return flow_removed_;
         }
 
     private:
