8e1e1f43d138218341e6336ab7a4229bfaf18a57
amedama41 <kamo.tanabota@gmail.com>
2016-03-05 22:33:54 +0900

    fix set_field test

--- test/v13/action/set_field_test.cpp
+++ test/v13/action/set_field_test.cpp
@@ -4,7 +4,12 @@
 #include <cstdint>
 #include <vector>
 #include <utility>
+#include <boost/asio/ip/address_v4.hpp>
+#include <boost/asio/ip/address_v6.hpp>
 #include <canard/network/protocol/openflow/v13/oxm_match_field.hpp>
+
+using boost::asio::ip::address_v4;
+using boost::asio::ip::address_v6;
 
 namespace canard {
 namespace network {
@@ -17,7 +22,7 @@ BOOST_AUTO_TEST_SUITE(instantiation_test)
 
     BOOST_AUTO_TEST_CASE(constructor_test)
     {
-        auto const oxm = oxm_eth_type{0x0806};
+        auto const oxm = oxm_match::eth_type{0x0806};
 
         auto const sut = actions::set_field{oxm};
 
@@ -25,14 +30,14 @@ BOOST_AUTO_TEST_SUITE(instantiation_test)
         BOOST_CHECK_EQUAL(sut.length(), ((4 + oxm.length()) + 7) / 8 * 8);
         BOOST_CHECK_EQUAL(sut.length() % 8, 0);
         BOOST_CHECK_EQUAL(sut.oxm_match_field().oxm_type(), oxm.oxm_type());
-        auto const sut_field = any_cast<oxm_eth_type>(&sut.oxm_match_field());
+        auto const sut_field = any_cast<oxm_match::eth_type>(&sut.oxm_match_field());
         BOOST_REQUIRE(sut_field);
         BOOST_CHECK_EQUAL(sut_field->oxm_value(), oxm.oxm_value());
     }
 
     BOOST_AUTO_TEST_CASE(copy_constructor_test)
     {
-        auto sut = actions::set_field{oxm_ipv6_src{{{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0xd, 0x0e, 0x0f}}}};
+        auto sut = actions::set_field{oxm_match::ipv6_src{address_v6{{{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0xd, 0x0e, 0x0f}}}}};
 
         auto const copy = sut;
 
@@ -42,15 +47,15 @@ BOOST_AUTO_TEST_SUITE(instantiation_test)
 
     BOOST_AUTO_TEST_CASE(construct_with_any_oxm_match_field)
     {
-        auto oxm = any_oxm_match_field{oxm_vlan_vid{0x0fff}};
+        auto oxm = any_oxm_match_field{oxm_match::vlan_vid{0x0fff}};
 
         auto const sut = actions::set_field{std::move(oxm)};
 
         BOOST_CHECK_EQUAL(sut.type(), protocol::OFPAT_SET_FIELD);
         BOOST_CHECK_EQUAL(sut.length(), ((4 + sizeof(std::uint32_t) + sizeof(std::uint16_t)) + 7) / 8 * 8);
         BOOST_CHECK_EQUAL(sut.length() % 8, 0);
-        BOOST_CHECK_EQUAL(sut.oxm_match_field().oxm_type(), oxm_vlan_vid::oxm_type());
-        auto const sut_field = any_cast<oxm_vlan_vid>(&sut.oxm_match_field());
+        BOOST_CHECK_EQUAL(sut.oxm_match_field().oxm_type(), oxm_match::vlan_vid::oxm_type());
+        auto const sut_field = any_cast<oxm_match::vlan_vid>(&sut.oxm_match_field());
         BOOST_REQUIRE(sut_field);
         BOOST_CHECK_EQUAL(sut_field->oxm_value(), 0x0fff);
     }
@@ -69,32 +74,32 @@ BOOST_AUTO_TEST_SUITE(assignment_test)
 
     BOOST_AUTO_TEST_CASE(copy_assign_test)
     {
-        auto sut = actions::set_field{oxm_ipv4_src{0x7f000001}};
-        auto copy = actions::set_field{oxm_ipv4_src{0x7f000002}};
+        auto sut = actions::set_field{oxm_match::ipv4_src{address_v4{0x7f000001}}};
+        auto copy = actions::set_field{oxm_match::ipv4_src{address_v4{0x7f000002}}};
 
         copy = sut;
 
         BOOST_CHECK_EQUAL(copy.type(), sut.type());
         BOOST_CHECK_EQUAL(copy.length(), sut.length());
-        BOOST_CHECK_EQUAL(copy.oxm_match_field().oxm_type(), oxm_ipv4_src::oxm_type());
-        auto const field = any_cast<oxm_ipv4_src>(&copy.oxm_match_field());
+        BOOST_CHECK_EQUAL(copy.oxm_match_field().oxm_type(), oxm_match::ipv4_src::oxm_type());
+        auto const field = any_cast<oxm_match::ipv4_src>(&copy.oxm_match_field());
         BOOST_REQUIRE(field);
-        BOOST_CHECK_EQUAL(field->oxm_value(), 0x7f000001);
+        BOOST_CHECK_EQUAL(field->oxm_value(), address_v4{0x7f000001});
     }
 
     BOOST_AUTO_TEST_CASE(move_assign_test)
     {
-        auto sut = actions::set_field{oxm_ipv4_src{0x7f000001}};
-        auto copy = actions::set_field{oxm_ipv4_src{0x7f000002}};
+        auto sut = actions::set_field{oxm_match::ipv4_src{address_v4{0x7f000001}}};
+        auto copy = actions::set_field{oxm_match::ipv4_src{address_v4{0x7f000002}}};
 
         copy = std::move(sut);
 
         BOOST_CHECK_EQUAL(copy.type(), sut.type());
         BOOST_CHECK_EQUAL(copy.length(), sut.length());
-        BOOST_CHECK_EQUAL(copy.oxm_match_field().oxm_type(), oxm_ipv4_src::oxm_type());
-        auto const field = any_cast<oxm_ipv4_src>(&copy.oxm_match_field());
+        BOOST_CHECK_EQUAL(copy.oxm_match_field().oxm_type(), oxm_match::ipv4_src::oxm_type());
+        auto const field = any_cast<oxm_match::ipv4_src>(&copy.oxm_match_field());
         BOOST_REQUIRE(field);
-        BOOST_CHECK_EQUAL(field->oxm_value(), 0x7f000001);
+        BOOST_CHECK_EQUAL(field->oxm_value(), address_v4{0x7f000001});
     }
 
 BOOST_AUTO_TEST_SUITE_END() // assignment_test
@@ -102,7 +107,7 @@ BOOST_AUTO_TEST_SUITE_END() // assignment_test
 BOOST_AUTO_TEST_CASE(encode_decode_test)
 {
     auto buffer = std::vector<std::uint8_t>{};
-    auto const sut = actions::set_field{oxm_ipv4_src{0x7f000001}};
+    auto const sut = actions::set_field{oxm_match::ipv4_src{address_v4{0x7f000001}}};
 
     sut.encode(buffer);
 
@@ -114,10 +119,10 @@ BOOST_AUTO_TEST_CASE(encode_decode_test)
     BOOST_CHECK(it == buffer.end());
     BOOST_CHECK_EQUAL(decoded_action.type(), sut.type());
     BOOST_CHECK_EQUAL(decoded_action.length(), sut.length());
-    BOOST_CHECK_EQUAL(decoded_action.oxm_match_field().oxm_type(), oxm_ipv4_src::oxm_type());
-    auto const decode_action_field = any_cast<oxm_ipv4_src>(&sut.oxm_match_field());
+    BOOST_CHECK_EQUAL(decoded_action.oxm_match_field().oxm_type(), oxm_match::ipv4_src::oxm_type());
+    auto const decode_action_field = any_cast<oxm_match::ipv4_src>(&sut.oxm_match_field());
     BOOST_REQUIRE(decode_action_field);
-    BOOST_CHECK_EQUAL(decode_action_field->oxm_value(), 0x7f000001);
+    BOOST_CHECK_EQUAL(decode_action_field->oxm_value(), address_v4{0x7f000001});
 }
 
 BOOST_AUTO_TEST_SUITE_END() // set_field_test
