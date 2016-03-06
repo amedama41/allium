44739d4761fe6b0ee2c6af87f18083d0302bdc85
amedama41 <kamo.tanabota@gmail.com>
2016-03-06 15:01:37 +0900

    rewrite v13 set_mpls_ttl action type

--- include/canard/network/protocol/openflow/v13/action/set_mpls_ttl.hpp
+++ include/canard/network/protocol/openflow/v13/action/set_mpls_ttl.hpp
@@ -1,85 +1,74 @@
-#ifndef CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_MPLS_TTL_HPP
-#define CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_MPLS_TTL_HPP
+#ifndef CANARD_NETWORK_OPENFLOW_V13_ACTIONS_SET_MPLS_TTL_HPP
+#define CANARD_NETWORK_OPENFLOW_V13_ACTIONS_SET_MPLS_TTL_HPP
 
 #include <cstdint>
-#include <canard/network/protocol/openflow/detail/decode.hpp>
-#include <canard/network/protocol/openflow/detail/encode.hpp>
-#include <canard/network/protocol/openflow/v13/detail/byteorder.hpp>
+#include <canard/network/protocol/openflow/v13/detail/basic_action.hpp>
 #include <canard/network/protocol/openflow/v13/openflow.hpp>
 
 namespace canard {
 namespace network {
 namespace openflow {
 namespace v13 {
+namespace actions {
 
-    namespace actions {
+    class set_mpls_ttl
+        : public detail::v13::basic_action<
+            set_mpls_ttl, v13_detail::ofp_action_mpls_ttl
+          >
+    {
+    public:
+        static constexpr protocol::ofp_action_type action_type
+            = protocol::OFPAT_SET_MPLS_TTL;
 
-        class set_mpls_ttl
+        explicit set_mpls_ttl(std::uint8_t const mpls_ttl) noexcept
+            : action_mpls_ttl_{
+                  action_type
+                , length()
+                , mpls_ttl
+                , { 0, 0, 0 }
+              }
         {
-        public:
-            static protocol::ofp_action_type const action_type
-                = protocol::OFPAT_SET_MPLS_TTL;
+        }
 
-            explicit set_mpls_ttl(std::uint8_t const mpls_ttl)
-                : mpls_ttl_{action_type, sizeof(v13_detail::ofp_action_mpls_ttl), mpls_ttl}
-            {
-            }
+        auto ttl() const noexcept
+            -> std::uint8_t
+        {
+            return action_mpls_ttl_.mpls_ttl;
+        }
 
-            auto type() const
-                -> protocol::ofp_action_type
-            {
-                return action_type;
-            }
+    private:
+        friend basic_action;
 
-            auto length() const
-                -> std::uint16_t
-            {
-                return sizeof(v13_detail::ofp_action_mpls_ttl);
-            }
+        explicit set_mpls_ttl(raw_ofp_type const& action_mpls_ttl) noexcept
+            : action_mpls_ttl_(action_mpls_ttl)
+        {
+        }
 
-            auto ttl() const
-                -> std::uint8_t
-            {
-                return mpls_ttl_.mpls_ttl;
-            }
+        auto ofp_action() const noexcept
+            -> raw_ofp_type const&
+        {
+            return action_mpls_ttl_;
+        }
 
-            template <class Container>
-            auto encode(Container& container) const
-                -> Container&
-            {
-                return detail::encode(container, mpls_ttl_);
-            }
+        static void validate_impl(set_mpls_ttl const&)
+        {
+        }
 
-        private:
-            explicit set_mpls_ttl(v13_detail::ofp_action_mpls_ttl const& action_mpls_ttl)
-                : mpls_ttl_(action_mpls_ttl)
-            {
-                if (mpls_ttl_.type != type()) {
-                    throw 1;
-                }
-                if (mpls_ttl_.len != length()) {
-                    throw 2;
-                }
-            }
+    private:
+        raw_ofp_type action_mpls_ttl_;
+    };
 
-        public:
-            template <class Iterator>
-            static auto decode(Iterator& first, Iterator last)
-                -> set_mpls_ttl
-            {
-                auto const action_mpls_ttl = detail::decode<v13_detail::ofp_action_mpls_ttl>(first, last);
-                return set_mpls_ttl{action_mpls_ttl};
-            }
+    inline auto operator==(
+            set_mpls_ttl const& lhs, set_mpls_ttl const& rhs) noexcept
+        -> bool
+    {
+        return lhs.ttl() == rhs.ttl();
+    }
 
-        private:
-            v13_detail::ofp_action_mpls_ttl mpls_ttl_;
-        };
-
-    } // namespace actions
-
+} // namespace actions
 } // namespace v13
 } // namespace openflow
 } // namespace network
 } // namespace canard
 
-#endif // CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_MPLS_TTL_HPP
+#endif // CANARD_NETWORK_OPENFLOW_V13_ACTIONS_SET_MPLS_TTL_HPP
