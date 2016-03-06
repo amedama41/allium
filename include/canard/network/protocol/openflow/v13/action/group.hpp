32a362d6b20db997f3612eb17ec8e92e97d58778
amedama41 <kamo.tanabota@gmail.com>
2016-03-06 15:00:29 +0900

    rewrite v13 group action type

--- include/canard/network/protocol/openflow/v13/action/group.hpp
+++ include/canard/network/protocol/openflow/v13/action/group.hpp
@@ -1,91 +1,75 @@
-#ifndef CANARD_NETWORK_OPENFLOW_V13_ACTION_GROUP_HPP
-#define CANARD_NETWORK_OPENFLOW_V13_ACTION_GROUP_HPP
+#ifndef CANARD_NETWORK_OPENFLOW_V13_ACTIONS_GROUP_HPP
+#define CANARD_NETWORK_OPENFLOW_V13_ACTIONS_GROUP_HPP
 
 #include <cstdint>
-#include <canard/network/protocol/openflow/detail/decode.hpp>
-#include <canard/network/protocol/openflow/detail/encode.hpp>
-#include <canard/network/protocol/openflow/v13/detail/byteorder.hpp>
+#include <stdexcept>
+#include <canard/network/protocol/openflow/v13/detail/basic_action.hpp>
 #include <canard/network/protocol/openflow/v13/openflow.hpp>
 
 namespace canard {
 namespace network {
 namespace openflow {
 namespace v13 {
+namespace actions {
 
-    namespace actions {
+    class group
+        : public detail::v13::basic_action<group, v13_detail::ofp_action_group>
+    {
+    public:
+        static constexpr protocol::ofp_action_type action_type
+            = protocol::OFPAT_GROUP;
 
-        class group
+        explicit group(std::uint32_t const group_id) noexcept
+            : action_group_{
+                  action_type
+                , length()
+                , group_id
+              }
         {
-        public:
-            static protocol::ofp_action_type const action_type
-                = protocol::OFPAT_GROUP;
-
-            explicit group(std::uint32_t const group_id)
-                : group_{action_type, sizeof(v13_detail::ofp_action_group), group_id}
-            {
-                if (group_id > protocol::OFPG_MAX) {
-                    throw 3;
-                }
-            };
-
-            auto type() const
-                -> protocol::ofp_action_type
-            {
-                return action_type;
-            }
-
-            auto length() const
-                -> std::uint16_t
-            {
-                return sizeof(v13_detail::ofp_action_group);
-            }
-
-            auto group_id() const
-                -> std::uint32_t
-            {
-                return group_.group_id;
-            }
-
-            template <class Container>
-            auto encode(Container& container) const
-                -> Container&
-            {
-                return detail::encode(container, group_);
-            }
-
-        private:
-            explicit group(v13_detail::ofp_action_group const& action_group)
-                : group_(action_group)
-            {
-                if (group_.type != action_type) {
-                    throw 1;
-                }
-                if (group_.len != sizeof(v13_detail::ofp_action_group)) {
-                    throw 2;
-                }
-                if (group_id() > protocol::OFPG_MAX) {
-                    throw 3;
-                }
-            }
-
-        public:
-            template <class Iterator>
-            static auto decode(Iterator& first, Iterator last)
-                -> group
-            {
-                auto const action_group = detail::decode<v13_detail::ofp_action_group>(first, last);
-                return group{action_group};
-            }
-
-        private:
-            v13_detail::ofp_action_group group_;
         };
 
-    } // namespace actions
+        auto group_id() const noexcept
+            -> std::uint32_t
+        {
+            return action_group_.group_id;
+        }
 
+    private:
+        friend basic_action;
+
+        explicit group(
+                v13_detail::ofp_action_group const& action_group) noexcept
+            : action_group_(action_group)
+        {
+        }
+
+        auto ofp_action() const noexcept
+            -> raw_ofp_type const&
+        {
+            return action_group_;
+        }
+
+        static void validate_impl(group const& action)
+        {
+            if (action.group_id() > protocol::OFPG_MAX) {
+                throw std::runtime_error{"invalid group_id"};
+            }
+        }
+
+    private:
+        raw_ofp_type action_group_;
+    };
+
+    inline auto operator==(group const& lhs, group const& rhs) noexcept
+        -> bool
+    {
+        return lhs.group_id() == rhs.group_id();
+    }
+
+} // namespace actions
 } // namespace v13
 } // namespace openflow
 } // namespace network
 } // namespace canard
 
-#endif // CANARD_NETWORK_OPENFLOW_V13_ACTION_GROUP_HPP
+#endif // CANARD_NETWORK_OPENFLOW_V13_ACTIONS_GROUP_HPP
