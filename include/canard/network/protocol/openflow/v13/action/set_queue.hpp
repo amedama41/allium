399660eb8d1d7a9a53335a26f408ca013036893b
amedama41 <kamo.tanabota@gmail.com>
2016-03-06 15:00:54 +0900

    rewrite v13 set_queue action type

--- include/canard/network/protocol/openflow/v13/action/set_queue.hpp
+++ include/canard/network/protocol/openflow/v13/action/set_queue.hpp
@@ -1,91 +1,76 @@
-#ifndef CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_QUEUE_HPP
-#define CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_QUEUE_HPP
+#ifndef CANARD_NETWORK_OPENFLOW_V13_ACTIONS_SET_QUEUE_HPP
+#define CANARD_NETWORK_OPENFLOW_V13_ACTIONS_SET_QUEUE_HPP
 
 #include <cstdint>
-#include <canard/network/protocol/openflow/detail/decode.hpp>
-#include <canard/network/protocol/openflow/detail/encode.hpp>
-#include <canard/network/protocol/openflow/v13/detail/byteorder.hpp>
+#include <stdexcept>
+#include <canard/network/protocol/openflow/v13/detail/basic_action.hpp>
 #include <canard/network/protocol/openflow/v13/openflow.hpp>
 
 namespace canard {
 namespace network {
 namespace openflow {
 namespace v13 {
+namespace actions {
 
-    namespace actions {
+    class set_queue
+        : public detail::v13::basic_action<
+            set_queue, v13_detail::ofp_action_set_queue
+          >
+    {
+    public:
+        static constexpr protocol::ofp_action_type action_type
+            = protocol::OFPAT_SET_QUEUE;
 
-        class set_queue
+        explicit set_queue(std::uint32_t const queue_id) noexcept
+            : action_set_queue_{
+                  action_type
+                , length()
+                , queue_id
+              }
         {
-        public:
-            static protocol::ofp_action_type const action_type
-                = protocol::OFPAT_SET_QUEUE;
+        }
 
-            explicit set_queue(std::uint32_t const queue_id)
-                : set_queue_{action_type, sizeof(v13_detail::ofp_action_set_queue), queue_id}
-            {
-                if (queue_id == protocol::OFPQ_ALL) {
-                    throw 3;
-                }
+        auto queue_id() const noexcept
+            -> std::uint32_t
+        {
+            return action_set_queue_.queue_id;
+        }
+
+    private:
+        friend basic_action;
+
+        explicit set_queue(raw_ofp_type const& action_set_queue) noexcept
+            : action_set_queue_(action_set_queue)
+        {
+        }
+
+        auto ofp_action() const noexcept
+            -> raw_ofp_type const&
+        {
+            return action_set_queue_;
+        }
+
+        static void validate_impl(set_queue const& action)
+        {
+            if (action.queue_id() == protocol::OFPQ_ALL) {
+                throw std::runtime_error{"invalid queue_id"};
             }
+        }
 
-            auto type() const
-                -> protocol::ofp_action_type
-            {
-                return action_type;
-            }
+    private:
+        raw_ofp_type action_set_queue_;
+    };
 
-            auto length() const
-                -> std::uint16_t
-            {
-                return sizeof(v13_detail::ofp_action_set_queue);
-            }
+    inline auto operator==(set_queue const& lhs, set_queue const& rhs) noexcept
+        -> bool
+    {
+        return lhs.queue_id() == rhs.queue_id();
+    }
 
-            auto queue_id() const
-                -> std::uint32_t
-            {
-                return set_queue_.queue_id;
-            }
-
-            template <class Container>
-            auto encode(Container& container) const
-                -> Container&
-            {
-                return detail::encode(container, set_queue_);
-            }
-
-        private:
-            explicit set_queue(v13_detail::ofp_action_set_queue const& action_set_queue)
-                : set_queue_(action_set_queue)
-            {
-                if (set_queue_.type != type()) {
-                    throw 1;
-                }
-                if (set_queue_.len != length()) {
-                    throw 2;
-                }
-                if (set_queue_.queue_id == protocol::OFPQ_ALL) {
-                    throw 3;
-                }
-            }
-
-        public:
-            template <class Iterator>
-            static auto decode(Iterator& first, Iterator last)
-                -> set_queue
-            {
-                auto const action_set_queue = detail::decode<v13_detail::ofp_action_set_queue>(first, last);
-                return set_queue{action_set_queue};
-            }
-
-        private:
-            v13_detail::ofp_action_set_queue set_queue_;
-        };
-
-    } // namespace actions
-
+} // namespace actions
 } // namespace v13
 } // namespace openflow
 } // namespace network
 } // namespace canard
 
-#endif // CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_QUEUE_HPP
+#endif // CANARD_NETWORK_OPENFLOW_V13_ACTIONS_SET_QUEUE_HPP
