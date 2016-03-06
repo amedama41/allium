8fe8169e2a5724499b351d4e87a895e031d6d423
amedama41 <kamo.tanabota@gmail.com>
2016-03-06 15:01:58 +0900

    rewrite v13 set_nw_ttl action type

--- include/canard/network/protocol/openflow/v13/action/set_nw_ttl.hpp
+++ include/canard/network/protocol/openflow/v13/action/set_nw_ttl.hpp
@@ -1,79 +1,74 @@
-#ifndef CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_NW_TTL_HPP
-#define CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_NW_TTL_HPP
+#ifndef CANARD_NETWORK_OPENFLOW_V13_ACTIONS_SET_NW_TTL_HPP
+#define CANARD_NETWORK_OPENFLOW_V13_ACTIONS_SET_NW_TTL_HPP
 
 #include <cstdint>
-#include <canard/network/protocol/openflow/detail/decode.hpp>
-#include <canard/network/protocol/openflow/detail/encode.hpp>
-#include <canard/network/protocol/openflow/v13/detail/byteorder.hpp>
+#include <canard/network/protocol/openflow/v13/detail/basic_action.hpp>
 #include <canard/network/protocol/openflow/v13/openflow.hpp>
 
 namespace canard {
 namespace network {
 namespace openflow {
 namespace v13 {
+namespace actions {
 
-    namespace actions {
+    class set_nw_ttl
+        : public detail::v13::basic_action<
+            set_nw_ttl, v13_detail::ofp_action_nw_ttl
+          >
+    {
+    public:
+        static constexpr protocol::ofp_action_type action_type
+            = protocol::OFPAT_SET_NW_TTL;
 
-        class set_nw_ttl
+        explicit set_nw_ttl(std::uint8_t const nw_ttl) noexcept
+            : action_nw_ttl_{
+                  action_type
+                , length()
+                , nw_ttl
+                , { 0, 0, 0 }
+              }
         {
-        public:
-            static protocol::ofp_action_type const action_type
-                = protocol::OFPAT_SET_NW_TTL;
+        }
 
-            explicit set_nw_ttl(std::uint8_t const nw_ttl)
-                : nw_ttl_{action_type, sizeof(v13_detail::ofp_action_nw_ttl), nw_ttl, {0}}
-            {
-            }
+        auto ttl() const noexcept
+            -> std::uint8_t
+        {
+            return action_nw_ttl_.nw_ttl;
+        }
 
-            auto type() const
-                -> protocol::ofp_action_type
-            {
-                return action_type;
-            }
+    private:
+        friend basic_action;
 
-            auto length() const
-                -> std::uint16_t
-            {
-                return sizeof(v13_detail::ofp_action_nw_ttl);
-            }
+        explicit set_nw_ttl(raw_ofp_type const& action_nw_ttl) noexcept
+            : action_nw_ttl_(action_nw_ttl)
+        {
+        }
 
-            auto ttl() const
-                -> std::uint8_t
-            {
-                return nw_ttl_.nw_ttl;
-            }
+        auto ofp_action() const noexcept
+            -> raw_ofp_type const&
+        {
+            return action_nw_ttl_;
+        }
 
-            template <class Container>
-            auto encode(Container& container) const
-                -> Container&
-            {
-                return detail::encode(container, nw_ttl_);
-            }
+        static void validate_impl(set_nw_ttl const&)
+        {
+        }
 
-        public:
-            template <class Iterator>
-            static auto decode(Iterator& first, Iterator last)
-                -> set_nw_ttl
-            {
-                auto const action_nw_ttl = detail::decode<v13_detail::ofp_action_nw_ttl>(first, last);
-                if (action_nw_ttl.type != action_type) {
-                    throw 1;
-                }
-                if (action_nw_ttl.len != sizeof(v13_detail::ofp_action_nw_ttl)) {
-                    throw 2;
-                }
-                return set_nw_ttl{action_nw_ttl.nw_ttl};
-            }
+    private:
+        raw_ofp_type action_nw_ttl_;
+    };
 
-        private:
-            v13_detail::ofp_action_nw_ttl nw_ttl_;
-        };
+    inline auto operator==(
+            set_nw_ttl const& lhs, set_nw_ttl const& rhs) noexcept
+        -> bool
+    {
+        return lhs.ttl() == rhs.ttl();
+    }
 
-    } // namespace actions
-
+} // namespace actions
 } // namespace v13
 } // namespace openflow
 } // namespace network
 } // namespace canard
 
-#endif // CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_NW_TTL_HPP
+#endif // CANARD_NETWORK_OPENFLOW_V13_ACTIONS_SET_NW_TTL_HPP
