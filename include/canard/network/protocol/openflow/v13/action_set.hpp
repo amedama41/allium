85c98a801d116d8200f12aabbc268938468e14eb
amedama41 <kamo.tanabota@gmail.com>
2016-03-19 22:06:59 +0900

    rewrite action_set class

--- include/canard/network/protocol/openflow/v13/action_set.hpp
+++ include/canard/network/protocol/openflow/v13/action_set.hpp
@@ -2,17 +2,16 @@
 #define CANARD_NETWORK_OPENFLOW_V13_ACTION_SET_HPP
 
 #include <cstdint>
-#include <map>
+#include <stdexcept>
 #include <type_traits>
 #include <utility>
-#include <boost/range/adaptor/map.hpp>
-#include <boost/range/adaptor/transformed.hpp>
-#include <boost/range/algorithm/for_each.hpp>
-#include <boost/range/numeric.hpp>
+#include <boost/operators.hpp>
+#include <boost/optional/optional.hpp>
+#include <boost/range/algorithm/find_if.hpp>
+#include <boost/range/algorithm/lower_bound.hpp>
+#include <canard/network/protocol/openflow/v13/action_list.hpp>
+#include <canard/network/protocol/openflow/v13/actions.hpp>
 #include <canard/type_traits.hpp>
-#include <canard/network/protocol/openflow/detail/add_helper.hpp>
-#include <canard/network/protocol/openflow/v13/any_action.hpp>
-#include <canard/network/protocol/openflow/v13/decoder/action_decoder.hpp>
 
 namespace canard {
 namespace network {
@@ -20,81 +19,260 @@ namespace openflow {
 namespace v13 {
 
     class action_set
+        : private boost::equality_comparable<action_set>
     {
     public:
-        action_set()
-            : action_map_{}
+        using key_type = std::uint64_t;
+        using value_type = action_list::value_type;
+        using reference = action_list::const_reference;
+        using const_reference = action_list::const_reference;
+        using iterator = action_list::const_iterator;
+        using const_iterator = action_list::const_iterator;
+        using difference_type = action_list::difference_type;
+        using size_type = action_list::size_type;
+        using reverse_iterator = action_list::reverse_iterator;
+        using const_reverse_iterator = action_list::const_reverse_iterator;
+
+        action_set() = default;
+
+        template <
+              class... Actions
+            , typename std::enable_if<
+                !canard::is_related<action_set, Actions...>::value
+              >::type* = nullptr
+        >
+        action_set(Actions&&... actions)
         {
+            std::pair<const_iterator, bool> dummy[] = {
+                insert(std::forward<Actions>(actions))...
+            };
+            static_cast<void>(dummy);
         }
 
-        template <class... Actions, typename std::enable_if<!is_related<action_set, Actions...>::value>::type* = nullptr>
-        action_set(Actions&&... actions)
-            : action_map_{}
+        auto begin() const noexcept
+            -> const_iterator
         {
-            add_impl(std::forward<Actions>(actions)...);
+            return actions_.begin();
+        }
+
+        auto end() const noexcept
+            -> const_iterator
+        {
+            return actions_.end();
+        }
+
+        auto cbegin() const noexcept
+            -> const_iterator
+        {
+            return actions_.cbegin();
+        }
+
+        auto cend() const noexcept
+            -> const_iterator
+        {
+            return actions_.cend();
+        }
+
+        auto rbegin() const noexcept
+            -> const_reverse_iterator
+        {
+            return actions_.rbegin();
+        }
+
+        auto rend() const noexcept
+            -> const_reverse_iterator
+        {
+            return actions_.rend();
+        }
+
+        auto crbegin() const noexcept
+            -> const_reverse_iterator
+        {
+            return actions_.crbegin();
+        }
+
+        auto crend() const noexcept
+            -> const_reverse_iterator
+        {
+            return actions_.crend();
+        }
+
+        auto empty() const noexcept
+            -> bool
+        {
+            return actions_.empty();
+        }
+
+        auto size() const noexcept
+            -> size_type
+        {
+            return actions_.size();
+        }
+
+        auto max_size() const noexcept
+            -> size_type
+        {
+            return actions_.max_size();
+        }
+
+        auto at(key_type const act_order) const
+            -> const_reference
+        {
+            auto const it = find(act_order);
+            if (it == actions_.end()) {
+                throw std::out_of_range{"not found specified action"};
+            }
+            return *it;
         }
 
         template <class Action>
-        void add(Action&& action)
+        auto at() const
+            -> Action const&
         {
-            auto const order = action_order(action);
-            auto const it = action_map_.lower_bound(order);
-            if (it != action_map_.end() && !action_map_.key_comp()(order, it->first)) {
-                it->second = std::forward<Action>(action);
+            return v13::any_cast<Action>(at(action_order<Action>::value));
+        }
+
+        template <class Action>
+        auto get() const
+            -> Action const&
+        {
+            auto const it = find(action_order<Action>::value);
+            return v13::any_cast<Action>(*it);
+        }
+
+        template <class Action>
+        auto insert(Action&& action)
+            -> std::pair<const_iterator, bool>
+        {
+            auto const order = get_order(action);
+            auto const pos = non_const_lower_bound(order);
+            if (pos != actions_.end() && get_order(*pos) == order) {
+                return std::make_pair(const_iterator{pos}, false);
             }
             else {
-                action_map_.emplace_hint(it, order, std::forward<Action>(action));
+                auto const it = actions_.insert(
+                        pos, value_type{std::forward<Action>(action)});
+                return std::make_pair(const_iterator{it}, true);
             }
+        }
+
+        template <class Action>
+        auto assign(Action&& action)
+            -> std::pair<const_iterator, bool>
+        {
+            auto const order = get_order(action);
+            auto const pos = non_const_lower_bound(order);
+            if (pos != actions_.end() && get_order(*pos) == order) {
+                *pos = std::forward<Action>(action);
+                return std::make_pair(const_iterator{pos}, true);
+            }
+            else {
+                return std::make_pair(const_iterator{actions_.end()}, false);
+            }
+        }
+
+        template <class Action>
+        auto insert_or_assign(Action&& action)
+            -> std::pair<const_iterator, bool>
+        {
+            auto const order = get_order(action);
+            auto const pos = non_const_lower_bound(order);
+            if (pos != actions_.end() && get_order(*pos) == order) {
+                *pos = std::forward<Action>(action);
+                return std::make_pair(const_iterator{pos}, false);
+            }
+            else {
+                auto const it = actions_.insert(
+                        pos, value_type{std::forward<Action>(action)});
+                return std::make_pair(const_iterator{it}, true);
+            }
+        }
+
+        auto erase(const_iterator it)
+            -> const_iterator
+        {
+            return actions_.erase(it);
+        }
+
+        template <class Action>
+        auto erase()
+            -> size_type
+        {
+            auto const it = find(action_order<Action>::value);
+            if (it == actions_.end()) {
+                return 0;
+            }
+            actions_.erase(it);
+            return 1;
+        }
+
+        void swap(action_set& other) noexcept
+        {
+            actions_.swap(other.actions_);
+        }
+
+        void clear() noexcept
+        {
+            return actions_.clear();
+        }
+
+        auto find(key_type const act_order) const
+            -> const_iterator
+        {
+            return boost::find_if(actions_, [=](const_reference e) {
+                    return get_order(e) == act_order;
+            });
+        }
+
+        template <class Action>
+        auto find() const
+            -> boost::optional<Action const&>
+        {
+            auto const it = find(action_order<Action>::value);
+            if (it == actions_.end()) {
+                return boost::none;
+            }
+            return v13::any_cast<Action>(*it);
         }
 
         auto length() const
             -> std::uint16_t
         {
-            using boost::adaptors::map_values;
-            using boost::adaptors::transformed;
-            return boost::accumulate(action_map_ | map_values | transformed([](any_action const& action) {
-                    return action.length();
-            }), std::uint16_t{0});
+            return actions_.length();
         }
 
-        template <class Container>
-        auto encode(Container& container) const
-            -> Container&
+        auto to_list() const&
+            -> action_list const&
         {
-            using boost::adaptors::map_values;
-            boost::for_each(action_map_ | map_values, [&](any_action const& action) {
-                action.encode(container);
-            });
-            return container;
+            return actions_;
         }
 
-        template <class Iterator>
-        static auto decode(Iterator& first, Iterator last)
-            -> action_set
+        auto to_list() &&
+            -> action_list
         {
-            auto act_set = action_set{};
-            while (first != last) {
-                action_decoder::decode<void>(
-                        first, last, detail::add_helper<action_set>{act_set});
-            }
-            return act_set;
+            return actions_;
         }
 
     private:
-        void add_impl()
+        auto non_const_lower_bound(key_type const act_order)
+            -> action_list::iterator
         {
-        }
-
-        template <class Action, class... Actions>
-        void add_impl(Action&& action, Actions&&... actions)
-        {
-            add(std::forward<Action>(action));
-            add_impl(std::forward<Actions>(actions)...);
+            return boost::lower_bound(
+                      actions_
+                    , act_order
+                    , [](const_reference e, key_type const act_order)
+                      { return get_order(e) < act_order; });
         }
 
     private:
-        std::map<std::uint64_t, any_action> action_map_;
+        action_list actions_;
     };
+
+    auto operator==(action_set const& lhs, action_set const& rhs)
+        -> bool
+    {
+        return lhs.to_list() == rhs.to_list();
+    }
 
 } // namespace v13
 } // namespace openflow
