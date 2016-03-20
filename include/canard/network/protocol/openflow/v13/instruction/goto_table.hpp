c8a541bf336971b47e7f5cecdeaa71c28d4899d0
amedama41 <kamo.tanabota@gmail.com>
2016-03-20 13:19:13 +0900

    rewrite goto_table instruction type

--- include/canard/network/protocol/openflow/v13/instruction/goto_table.hpp
+++ include/canard/network/protocol/openflow/v13/instruction/goto_table.hpp
@@ -1,81 +1,77 @@
-#ifndef CANARD_NETWORK_OPENFLOW_V13_INSTRUCTION_GOTO_TABLE_HPP
-#define CANARD_NETWORK_OPENFLOW_V13_INSTRUCTION_GOTO_TABLE_HPP
+#ifndef CANARD_NETWORK_OPENFLOW_V13_INSTRUCTIONS_GOTO_TABLE_HPP
+#define CANARD_NETWORK_OPENFLOW_V13_INSTRUCTIONS_GOTO_TABLE_HPP
 
 #include <cstdint>
-#include <canard/network/protocol/openflow/detail/decode.hpp>
-#include <canard/network/protocol/openflow/detail/encode.hpp>
-#include <canard/network/protocol/openflow/v13/detail/byteorder.hpp>
+#include <stdexcept>
+#include <canard/network/protocol/openflow/v13/detail/basic_instruction.hpp>
 #include <canard/network/protocol/openflow/v13/openflow.hpp>
 
 namespace canard {
 namespace network {
 namespace openflow {
 namespace v13 {
+namespace instructions {
 
-    namespace instructions {
+    class goto_table
+        : public detail::v13::basic_instruction<
+            goto_table, v13_detail::ofp_instruction_goto_table
+          >
+    {
+    public:
+        static constexpr protocol::ofp_instruction_type instruction_type
+            = protocol::OFPIT_GOTO_TABLE;
 
-        class goto_table
+        explicit goto_table(std::uint8_t const table_id) noexcept
+            : instruction_goto_table_{
+                  instruction_type
+                , sizeof(raw_ofp_type)
+                , table_id
+                , { 0, 0, 0 }
+              }
         {
-        public:
-            static protocol::ofp_instruction_type const instruction_type
-                = protocol::OFPIT_GOTO_TABLE;
+        }
 
-            explicit goto_table(std::uint8_t const table_id)
-                : goto_table_{instruction_type, sizeof(v13_detail::ofp_instruction_goto_table), table_id, {0}}
-            {
-                if (table_id > protocol::OFPTT_MAX) {
-                    throw 3;
-                }
+        auto table_id() const noexcept
+            -> std::uint8_t
+        {
+            return instruction_goto_table_.table_id;
+        }
+
+    private:
+        friend basic_instruction;
+
+        explicit goto_table(raw_ofp_type const& instruction_goto_table) noexcept
+            : instruction_goto_table_(instruction_goto_table)
+        {
+        }
+
+        auto ofp_instruction() const noexcept
+            -> raw_ofp_type const&
+        {
+            return instruction_goto_table_;
+        }
+
+        static void validate_impl(goto_table const& goto_table)
+        {
+            if (goto_table.table_id() > protocol::OFPTT_MAX) {
+                throw std::runtime_error{"invalid table id"};
             }
+        }
 
-            auto type() const
-                -> protocol::ofp_instruction_type
-            {
-                return instruction_type;
-            }
+    private:
+        raw_ofp_type instruction_goto_table_;
+    };
 
-            auto length() const
-                -> std::uint16_t
-            {
-                return sizeof(v13_detail::ofp_instruction_goto_table);
-            }
+    auto operator==(goto_table const& lhs, goto_table const& rhs) noexcept
+        -> bool
+    {
+        return lhs.table_id() == rhs.table_id();
+    }
 
-            auto table_id() const
-                -> std::uint8_t
-            {
-                return goto_table_.table_id;
-            }
-
-            template <class Container>
-            auto encode(Container& container) const
-                -> Container&
-            {
-                return detail::encode(container, goto_table_);
-            }
-
-            template <class Iterator>
-            static auto decode(Iterator& first, Iterator last)
-                -> goto_table
-            {
-                auto const instruction_goto_table = detail::decode<v13_detail::ofp_instruction_goto_table>(first, last);
-                if (instruction_goto_table.type != instruction_type) {
-                    throw 1;
-                }
-                if (instruction_goto_table.len != sizeof(v13_detail::ofp_instruction_goto_table)) {
-                    throw 2;
-                }
-                return goto_table{instruction_goto_table.table_id};
-            }
-
-        private:
-            v13_detail::ofp_instruction_goto_table goto_table_;
-        };
-
-    } // namespace instructions
-
+} // namespace instructions
 } // namespace v13
 } // namespace openflow
 } // namespace network
 } // namespace canard
 
-#endif // CANARD_NETWORK_OPENFLOW_V13_INSTRUCTION_GOTO_TABLE_HPP
+#endif // CANARD_NETWORK_OPENFLOW_V13_INSTRUCTIONS_GOTO_TABLE_HPP
