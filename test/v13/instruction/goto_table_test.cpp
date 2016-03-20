356382e67c7aff8291d6cf1d90a3f13659d691ad
amedama41 <kamo.tanabota@gmail.com>
2016-03-20 13:15:45 +0900

    update goto_table test

--- test/v13/instruction/goto_table_test.cpp
+++ test/v13/instruction/goto_table_test.cpp
@@ -1,79 +1,102 @@
 #define BOOST_TEST_DYN_LINK
 #include <canard/network/protocol/openflow/v13/instruction/goto_table.hpp>
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/monomorphic.hpp>
+#include <boost/test/data/test_case.hpp>
+
 #include <cstdint>
-#include <utility>
+#include <stdexcept>
 #include <vector>
 
-namespace canard {
-namespace network {
-namespace openflow {
-namespace v13 {
+#include "../../test_utility.hpp"
 
+namespace of = canard::network::openflow;
+namespace v13 = of::v13;
+namespace instructions = v13::instructions;
+namespace detail = v13::v13_detail;
+
+namespace bdata = boost::unit_test::data;
+
+namespace {
+
+struct goto_table_fixture
+{
+    instructions::goto_table sut{0x12};
+    std::vector<unsigned char> binary = "\x00\x01\x00\x08\x12\x00\x00\x00"_bin;
+};
+
+}
+
+BOOST_AUTO_TEST_SUITE(instruction_test)
 BOOST_AUTO_TEST_SUITE(goto_table_test)
 
-BOOST_AUTO_TEST_SUITE(instantiation_test)
+    BOOST_AUTO_TEST_CASE(type_definition_test)
+    {
+        using sut = instructions::goto_table;
 
-BOOST_AUTO_TEST_CASE(constructor_test)
-{
-    auto const table_id = std::uint8_t{0};
+        BOOST_TEST(sut::type() == v13::protocol::OFPIT_GOTO_TABLE);
+        BOOST_TEST(sut::length() == sizeof(detail::ofp_instruction_goto_table));
+    }
 
-    auto const sut = instructions::goto_table{table_id};
+    BOOST_AUTO_TEST_CASE(construct_from_table_id_test)
+    {
+        auto const table_id = std::uint8_t{1};
 
-    BOOST_CHECK_EQUAL(sut.type(), protocol::OFPIT_GOTO_TABLE);
-    BOOST_CHECK_EQUAL(sut.length(), 8);
-    BOOST_CHECK_EQUAL(sut.table_id(), table_id);
-}
+        auto const sut = instructions::goto_table{table_id};
 
-BOOST_AUTO_TEST_CASE(copy_constructor_test)
-{
-    auto sut = instructions::goto_table{protocol::OFPTT_MAX};
+        BOOST_TEST(sut.table_id() == table_id);
+    }
 
-    auto const copy = sut;
+    BOOST_DATA_TEST_CASE(
+              create_success_test
+            , bdata::make(std::vector<std::uint8_t>{
+                0x00, v13::protocol::OFPTT_MAX
+              })
+            , table_id)
+    {
+        BOOST_CHECK_NO_THROW(instructions::goto_table::create(table_id));
+    }
 
-    BOOST_CHECK_EQUAL(copy.type(), sut.type());
-    BOOST_CHECK_EQUAL(copy.length(), sut.length());
-    BOOST_CHECK_EQUAL(copy.table_id(), sut.table_id());
-}
+    BOOST_DATA_TEST_CASE(
+              create_failure_test
+            , bdata::make(v13::protocol::OFPTT_ALL)
+            , table_id)
+    {
+        BOOST_CHECK_THROW(
+                  instructions::goto_table::create(table_id)
+                , std::runtime_error);
+    }
 
-BOOST_AUTO_TEST_CASE(move_constructor_test)
-{
-    auto sut = instructions::goto_table{protocol::OFPTT_MAX};
+    BOOST_AUTO_TEST_CASE(equality_test)
+    {
+        auto const sut1 = instructions::goto_table{0x12};
+        auto const sut2 = instructions::goto_table{0x13};
 
-    auto const copy = std::move(sut);
+        BOOST_TEST((sut1 == sut1));
+        BOOST_TEST((sut1 != sut2));
+        BOOST_TEST((sut2 == sut2));
+    }
 
-    BOOST_CHECK_EQUAL(copy.type(), sut.type());
-    BOOST_CHECK_EQUAL(copy.length(), sut.length());
-    BOOST_CHECK_EQUAL(copy.table_id(), sut.table_id());
-}
+    BOOST_FIXTURE_TEST_CASE(encode_test, goto_table_fixture)
+    {
+        auto buffer = std::vector<unsigned char>{};
 
-BOOST_AUTO_TEST_CASE(bod_table_id)
-{
-    BOOST_CHECK_THROW(instructions::goto_table{protocol::OFPTT_MAX + 1}, int);
-}
+        sut.encode(buffer);
 
-BOOST_AUTO_TEST_SUITE_END() // instantiation_test
+        BOOST_TEST(buffer.size() == sut.length());
+        BOOST_TEST(buffer == binary, boost::test_tools::per_element{});
+    }
 
-BOOST_AUTO_TEST_CASE(encode_decode_test)
-{
-    auto buffer = std::vector<std::uint8_t>{};
-    auto const sut = instructions::goto_table{1};
+    BOOST_FIXTURE_TEST_CASE(decode_test, goto_table_fixture)
+    {
+        auto it = binary.begin();
+        auto it_end = binary.end();
 
-    sut.encode(buffer);
+        auto const goto_table = instructions::goto_table::decode(it, it_end);
 
-    BOOST_CHECK_EQUAL(buffer.size(), sut.length());
-
-    auto it = buffer.begin();
-    auto const decoded_instruction = instructions::goto_table::decode(it, buffer.end());
-
-    BOOST_CHECK_EQUAL(decoded_instruction.type(), sut.type());
-    BOOST_CHECK_EQUAL(decoded_instruction.length(), sut.length());
-    BOOST_CHECK_EQUAL(decoded_instruction.table_id(), sut.table_id());
-}
+        BOOST_TEST((it == it_end));
+        BOOST_TEST((goto_table == sut));
+    }
 
 BOOST_AUTO_TEST_SUITE_END() // goto_table_test
-
-} // namespace v13
-} // namespace openflow
-} // namespace network
-} // namespace canard
+BOOST_AUTO_TEST_SUITE_END() // instruction_test
