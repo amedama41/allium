36b8330ab35946b288ad43520286580512874bb6
amedama41 <kamo.tanabota@gmail.com>
2016-03-20 13:17:52 +0900

    update meter test

--- test/v13/instruction/meter_test.cpp
+++ test/v13/instruction/meter_test.cpp
@@ -1,80 +1,106 @@
 #define BOOST_TEST_DYN_LINK
 #include <canard/network/protocol/openflow/v13/instruction/meter.hpp>
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/monomorphic.hpp>
+#include <boost/test/data/test_case.hpp>
+
 #include <cstdint>
-#include <utility>
 #include <vector>
 
-namespace canard {
-namespace network {
-namespace openflow {
-namespace v13 {
+#include "../../test_utility.hpp"
 
+namespace of = canard::network::openflow;
+namespace v13 = of::v13;
+namespace instructions = v13::instructions;
+namespace detail = v13::v13_detail;
+
+namespace bdata = boost::unit_test::data;
+
+namespace {
+
+struct meter_fixture
+{
+    instructions::meter sut{0x12345678};
+    std::vector<unsigned char> binary = "\x00\x06\x00\x08\x12\x34\x56\x78"_bin;
+};
+
+}
+
+BOOST_AUTO_TEST_SUITE(instruction_test)
 BOOST_AUTO_TEST_SUITE(meter_test)
 
-BOOST_AUTO_TEST_SUITE(instantiation_test)
+    BOOST_AUTO_TEST_CASE(type_definition_test)
+    {
+        using sut = instructions::meter;
 
-BOOST_AUTO_TEST_CASE(constructor_test)
-{
-    auto const meter_id = 1;
+        BOOST_TEST(sut::type() == v13::protocol::OFPIT_METER);
+        BOOST_TEST(sut::length() == sizeof(detail::ofp_instruction_meter));
+    }
 
-    auto const sut = instructions::meter{meter_id};
+    BOOST_AUTO_TEST_CASE(construct_from_meter_id_test)
+    {
+        auto const meter_id = std::uint32_t{0x12345678};
 
-    BOOST_CHECK_EQUAL(sut.type(), protocol::OFPIT_METER);
-    BOOST_CHECK_EQUAL(sut.length(), 8);
-    BOOST_CHECK_EQUAL(sut.meter_id(), meter_id);
-}
+        auto const sut = instructions::meter{meter_id};
 
-BOOST_AUTO_TEST_CASE(copy_constructor_test)
-{
-    auto sut = instructions::meter{protocol::OFPM_MAX};
+        BOOST_TEST(sut.meter_id() == meter_id);
+    }
 
-    auto const copy = sut;
+    BOOST_DATA_TEST_CASE(
+              create_success_test
+            , bdata::make(std::vector<std::uint32_t>{
+                  1, v13::protocol::OFPM_MAX
+              })
+            , meter_id)
+    {
+        BOOST_CHECK_NO_THROW(instructions::meter::create(meter_id));
+    }
 
-    BOOST_CHECK_EQUAL(copy.type(), sut.type());
-    BOOST_CHECK_EQUAL(copy.length(), sut.length());
-    BOOST_CHECK_EQUAL(copy.meter_id(), sut.meter_id());
-}
+    BOOST_DATA_TEST_CASE(
+              create_failure_test
+            , bdata::make(std::vector<std::uint32_t>{
+                  0
+                , v13::protocol::OFPM_SLOWPATH
+                , v13::protocol::OFPM_CONTROLLER
+                , v13::protocol::OFPM_ALL
+              })
+            , meter_id)
+    {
+        BOOST_CHECK_THROW(
+                  instructions::meter::create(meter_id)
+                , std::runtime_error);
+    }
 
-BOOST_AUTO_TEST_CASE(move_constructor_test)
-{
-    auto sut = instructions::meter{32};
+    BOOST_AUTO_TEST_CASE(equality_test)
+    {
+        auto const sut1 = instructions::meter{1};
+        auto const sut2 = instructions::meter{2};
 
-    auto const copy = std::move(sut);
+        BOOST_TEST((sut1 == sut1));
+        BOOST_TEST((sut1 != sut2));
+        BOOST_TEST((sut2 == sut2));
+    }
 
-    BOOST_CHECK_EQUAL(copy.type(), sut.type());
-    BOOST_CHECK_EQUAL(copy.length(), sut.length());
-    BOOST_CHECK_EQUAL(copy.meter_id(), sut.meter_id());
-}
+    BOOST_FIXTURE_TEST_CASE(encode_test, meter_fixture)
+    {
+        auto buffer = std::vector<unsigned char>{};
 
-BOOST_AUTO_TEST_CASE(bad_meter_id)
-{
-    BOOST_CHECK_THROW(instructions::meter{protocol::OFPM_MAX + 1}, int);
-    BOOST_CHECK_THROW(instructions::meter{protocol::OFPM_ALL}, int);
-}
+        sut.encode(buffer);
 
-BOOST_AUTO_TEST_SUITE_END() // instantiation_test
+        BOOST_TEST(buffer.size() == sut.length());
+        BOOST_TEST(buffer == binary, boost::test_tools::per_element{});
+    }
 
-BOOST_AUTO_TEST_CASE(encode_decode_test)
-{
-    auto buffer = std::vector<std::uint8_t>{};
-    auto const sut = instructions::meter{protocol::OFPM_MAX - 1};
+    BOOST_FIXTURE_TEST_CASE(decode_test, meter_fixture)
+    {
+        auto it = binary.begin();
+        auto it_end = binary.end();
 
-    sut.encode(buffer);
+        auto const meter = instructions::meter::decode(it, it_end);
 
-    BOOST_CHECK_EQUAL(buffer.size(), sut.length());
-
-    auto it = buffer.begin();
-    auto const decoded_instruction = instructions::meter::decode(it, buffer.end());
-
-    BOOST_CHECK_EQUAL(decoded_instruction.type(), sut.type());
-    BOOST_CHECK_EQUAL(decoded_instruction.length(), sut.length());
-    BOOST_CHECK_EQUAL(decoded_instruction.meter_id(), sut.meter_id());
-}
+        BOOST_TEST((it == it_end));
+        BOOST_TEST((meter == sut));
+    }
 
 BOOST_AUTO_TEST_SUITE_END() // meter_test
-
-} // namespace v13
-} // namespace openflow
-} // namespace network
-} // namespace canard
+BOOST_AUTO_TEST_SUITE_END() // instruction_test
