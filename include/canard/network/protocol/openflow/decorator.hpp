fd2ffbf705e00fabe84787a771366147572f9be9
amedama41 <kamo.tanabota@gmail.com>
2017-01-09 10:29:46 +0900

    redesign decorator framework

--- include/canard/network/protocol/openflow/decorator.hpp
+++ include/canard/network/protocol/openflow/decorator.hpp
@@ -15,232 +15,211 @@
 #include <boost/mpl/bool.hpp>
 #include <canard/integer_sequence.hpp>
 #include <canard/network/protocol/openflow/data_per_channel.hpp>
-#include <canard/type_traits.hpp>
 
 namespace canard {
 namespace network {
 namespace openflow {
 
-    template <std::size_t N, class Handler, class... Decorators>
+  namespace decorator_detail {
+
+    template <class Tuple>
+    using args_indices_t = canard::make_index_sequence<
+      std::tuple_size<typename std::decay<Tuple>::type>::value
+    >;
+
+    template <class Map, class Decorator>
+    using has_key_t
+      = typename boost::fusion::result_of::has_key<Map, Decorator>::type;
+
+    template <class Map, class Decorator>
+    using at_key_t
+      = typename boost::fusion::result_of::at_key<Map, Decorator>::type;
+
+    template <template <class> class T>
+    struct quote {};
+
+    template <class, class, template <class> class... >
+    struct decorate_impl;
+
+    template <class, std::size_t>
+    struct forwarder;
+
+    template <
+        class Handler
+      , class T, std::size_t... Ints, template <class> class... Decorators
+    >
+    auto get_all_decorators_impl(
+        decorate_impl<T, canard::index_sequence<Ints...>, Decorators...> const&)
+      -> std::tuple<T, Decorators<forwarder<T, Ints>>...>;
+    template <class Handler>
+    auto get_all_decorators_impl(...)
+      -> std::tuple<Handler>;
+
+    template <class T>
+    struct get_all_decorators
+    {
+      using type = decltype(get_all_decorators_impl<T>(std::declval<T>()));
+      static_assert(
+            std::is_same<typename std::tuple_element<0, type>::type, T>::value
+          , "Wrong use of decorate: "
+            "first parameter of decorate must be handler type");
+    };
+
+    template <class Handler, std::size_t NextDecoratorIndex>
     struct forwarder
     {
-        template <class Decorator, class... Args>
-        void operator()(Decorator* const d, Args&&... args) const
-        {
-            using next_decorator = typename std::tuple_element<
-                N, std::tuple<Handler, Decorators...>
-            >::type;
-            static_cast<next_decorator*>(static_cast<Handler*>(d))->handle(
-                      forwarder<N - 1, Handler, Decorators...>{}
-                    , std::forward<Args>(args)...);
-        }
+      template <class... Args>
+      void forward(Args&&... args)
+      {
+        using next_decorator = typename std::tuple_element<
+            NextDecoratorIndex, typename get_all_decorators<Handler>::type
+        >::type;
+        auto const next
+          = static_cast<next_decorator*>(static_cast<Handler*>(this));
+        next->next_decorator::handle(std::forward<Args>(args)...);
+      }
     };
 
-    template <class Handler, class... Decorators>
-    struct forwarder<0, Handler, Decorators...>
+    template <class Decorator>
+    struct decorator_wrapper
+      : Decorator
     {
-        template <class Decorator, class... Args>
-        void operator()(Decorator* const d, Args&&... args) const
-        {
-            static_cast<Handler*>(d)->handle(std::forward<Args>(args)...);
-        }
+      template <class Tuple>
+      decorator_wrapper(Tuple&& tuple)
+        : decorator_wrapper{std::forward<Tuple>(tuple), args_indices_t<Tuple>{}}
+      {
+      }
+
+      template <class Tuple, std::size_t... Ints>
+      decorator_wrapper(Tuple&& tuple, canard::index_sequence<Ints...>)
+        : Decorator{std::get<Ints>(std::forward<Tuple>(tuple))...}
+      {
+      }
     };
 
-    namespace decorator_detail {
+    template <bool B, class T>
+    using enable_if_t = typename std::enable_if<B, T>::type;
 
-        template <class Tuple>
-        using args_indices_t = canard::make_index_sequence<
-            std::tuple_size<canard::remove_cvref_t<Tuple>>::value
-        >;
-
-        template <class Map, class Decorator>
-        using has_key_t
-            = typename boost::fusion::result_of::has_key<Map, Decorator>::type;
-
-        template <class Map, class Decorator>
-        using at_key_t
-            = typename boost::fusion::result_of::at_key<Map, Decorator>::type;
-
-        template <class Decorator>
-        struct decorator_wrapper
-            : Decorator
-        {
-            template <class Tuple>
-            decorator_wrapper(Tuple&& tuple)
-                : decorator_wrapper{
-                    std::forward<Tuple>(tuple), args_indices_t<Tuple>{}
-                  }
-            {
-            }
-
-            template <class Tuple, std::size_t... Ints>
-            decorator_wrapper(Tuple&& tuple, canard::index_sequence<Ints...>)
-                : Decorator{std::get<Ints>(std::forward<Tuple>(tuple))...}
-            {
-            }
-        };
-
-        template <class Decorator, class Map>
-        auto get_args(Map& map)
-            -> typename std::enable_if<
-                    has_key_t<Map, Decorator>::value, at_key_t<Map, Decorator>
-               >::type
-        {
-            return boost::fusion::at_key<Decorator>(map);
-        }
-
-        template <class Decorator, class Map>
-        auto get_args(Map&)
-            -> typename std::enable_if<
-                    !has_key_t<Map, Decorator>::value, std::tuple<>
-               >::type
-        {
-            return std::tuple<>{};
-        }
-
-    } // namespace decorator_detail
-
-    template <class Decorator, class... Args>
-    auto make_args(Args&&... args)
-        -> boost::fusion::pair<Decorator, std::tuple<Args&&...>>
+    template <class Decorator, class Map>
+    auto get_args(Map& map)
+      -> enable_if_t<has_key_t<Map, Decorator>::value, at_key_t<Map, Decorator>>
     {
-        return boost::fusion::make_pair<Decorator>(
-                std::forward_as_tuple(std::forward<Args>(args)...));
+      return boost::fusion::at_key<Decorator>(map);
     }
 
-    template <class... Decorators>
-    struct decorate
-        : public decorator_detail::decorator_wrapper<Decorators>...
+    template <class Decorator, class Map>
+    auto get_args(Map&)
+      -> enable_if_t<!has_key_t<Map, Decorator>::value, std::tuple<>>
     {
-        template <class... Pairs>
-        explicit decorate(Pairs&&... pairs)
-            : decorate{
-                boost::fusion::map<canard::remove_cvref_t<Pairs>...>{
-                    std::forward<Pairs>(pairs)...
-                }
-              }
-        {
-        }
+      return std::tuple<>{};
+    }
+
+    template <
+      class Handler, std::size_t... Ints, template <class> class... Decorators
+    >
+    struct decorate_impl<
+      Handler, canard::index_sequence<Ints...>, Decorators...
+    >
+      : public decorator_wrapper<Decorators<forwarder<Handler, Ints>>>...
+    {
+      template <class... Pairs>
+      explicit decorate_impl(Pairs&&... pairs)
+        : decorate_impl{
+            boost::fusion::map<typename std::decay<Pairs>::type...>{
+              std::forward<Pairs>(pairs)...
+            }
+          }
+      {
+      }
 
     private:
-        template <class... Pairs>
-        explicit decorate(boost::fusion::map<Pairs...> map)
-            : decorator_detail::decorator_wrapper<Decorators>{
-                decorator_detail::get_args<Decorators>(map)
-              }...
-        {
-        }
+      template <class... Pairs>
+      explicit decorate_impl(boost::fusion::map<Pairs...> map)
+        : decorator_wrapper<Decorators<forwarder<Handler, Ints>>>{
+            get_args<quote<Decorators>>(map)
+          }...
+      {
+      }
     };
 
-    namespace detail {
 
-        template <class... Decorators>
-        auto has_decorators_impl(decorate<Decorators...>*) -> std::true_type;
-        auto has_decorators_impl(...) -> std::false_type;
+    template <class Decorator>
+    using get_channel_data_t = typename data_per_channel<Decorator>::type;
 
-        template <class Handler>
-        struct has_decorators
-        {
-            using type
-                = decltype(has_decorators_impl(static_cast<Handler*>(0)));
-        };
+    template <class Tuple>
+    struct to_pair_sequence;
 
-        template <class Handler>
-        using has_decorators_t = typename has_decorators<Handler>::type;
+    template <class... Decorators>
+    struct to_pair_sequence<std::tuple<Decorators...>>
+    {
+      using type = boost::fusion::vector<
+        boost::fusion::pair<Decorators, get_channel_data_t<Decorators>>...
+      >;
+    };
 
-        template <class... Decorators>
-        auto cast_to_decorator_impl(decorate<Decorators...> const& d)
-            -> decorate<Decorators...>&;
+    struct is_not_null
+    {
+      template <class T>
+      struct apply
+        : boost::mpl::bool_<!std::is_same<detail::null_data, T>::value>
+      {};
+    };
 
-        template <class Handler>
-        struct cast_to_decorator
-        {
-            using type = typename std::remove_reference<
-                decltype(cast_to_decorator_impl(std::declval<Handler>()))
-            >::type;
-        };
+  } // namespace decorator_detail
 
-        template <class T>
-        struct identity
-        {
-            using type = T;
-        };
+  namespace detail {
 
-        template <class Handler>
-        struct decorator
-            : std::conditional<
-                    has_decorators_t<Handler>::value
-                  , cast_to_decorator<Handler>
-                  , identity<decorate<>>
-              >::type
-        {
-        };
+    template <class Handler>
+    struct channel_data
+    {
+      using type = typename boost::fusion::result_of::as_map<
+        typename boost::fusion::result_of::filter_if<
+            typename decorator_detail::to_pair_sequence<
+              typename decorator_detail::get_all_decorators<Handler>::type
+            >::type
+          , decorator_detail::is_not_null
+        >::type
+      >::type;
+    };
 
-        template <class Handler>
-        using decorator_t = typename decorator<Handler>::type;
+    template <class Handler>
+    using channel_data_t = typename channel_data<Handler>::type;
 
-        template <class Decorator>
-        using to_pair_t
-            = boost::fusion::pair<Decorator, data_per_channel_t<Decorator>>;
+    template <class Handler, class... Args>
+    void handle(Handler& handler, Args&&... args)
+    {
+      using decorators
+        = typename decorator_detail::get_all_decorators<Handler>::type;
+      using lowest_decorator = typename std::tuple_element<
+        std::tuple_size<decorators>::value - 1, decorators
+      >::type;
+      static_cast<lowest_decorator&>(handler).handle(
+          std::forward<Args>(args)...);
+    }
 
-        struct is_not_null
-        {
-            template <class T>
-            struct apply
-                : boost::mpl::bool_<!std::is_same<detail::null_data, T>::value>
-            {};
-        };
+  } // namespace detail
 
-        template <class Handler, class Decorator>
-        struct channel_data;
+  template <template <class> class Decorator, class... Args>
+  auto make_args(Args&&... args)
+    -> boost::fusion::pair<
+         decorator_detail::quote<Decorator>, std::tuple<Args&&...>
+       >
+  {
+    return boost::fusion::make_pair<decorator_detail::quote<Decorator>>(
+        std::forward_as_tuple(std::forward<Args>(args)...));
+  }
 
-        template <class Handler, class... Decorators>
-        struct channel_data<Handler, decorate<Decorators...>>
-        {
-            using type = typename boost::fusion::result_of::as_map<
-                typename boost::fusion::result_of::filter_if<
-                      boost::fusion::vector<
-                            to_pair_t<Handler>, to_pair_t<Decorators>...>
-                    , is_not_null
-                >::type
-            >::type;
-        };
-
-        template <class Handler, class Decorator = decorator_t<Handler>>
-        using channel_data_t = typename channel_data<Handler, Decorator>::type;
-
-
-        template <class Handler, class Decorator = decorator_t<Handler>>
-        struct first_forwarder;
-
-        template <class Handler, class... Decorators>
-        struct first_forwarder<Handler, decorate<Decorators...>>
-        {
-            using type = forwarder<
-                  std::tuple_size<std::tuple<Decorators...>>::value
-                , Handler, Decorators...
-            >;
-        };
-
-        template <class Handler>
-        using first_forwarder_t = typename first_forwarder<Handler>::type;
-
-        template <class Handler, class... Args>
-        auto handle(Handler& handler, Args&&... args)
-            -> typename std::enable_if<has_decorators_t<Handler>::value>::type
-        {
-            first_forwarder_t<Handler>{}(
-                    std::addressof(handler), std::forward<Args>(args)...);
-        }
-
-        template <class Handler, class... Args>
-        auto handle(Handler& handler, Args&&... args)
-            -> typename std::enable_if<!has_decorators_t<Handler>::value>::type
-        {
-            handler.handle(std::forward<Args>(args)...);
-        }
-
-    } // namespace detail
+  template <class Handler, template <class> class... Decorators>
+  class decorate : public decorator_detail::decorate_impl<
+      Handler
+    , canard::make_index_sequence<sizeof...(Decorators)>
+    , Decorators...
+  >
+  {
+    using decorator_detail::decorate_impl<
+      Handler, canard::make_index_sequence<sizeof...(Decorators)>, Decorators...
+    >::decorate_impl;
+  };
 
 } // namespace openflow
 } // namespace network
